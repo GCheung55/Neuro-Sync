6b7d4ad9fe17f2c6a331eb84b82a357d4164e220
Garrick Cheung <garrick@garrickcheung.com>
2012-06-25 22:53:20 -0700

    Update sync to correctly handle and cancel one time sync events.

--- Source/Sync.js
+++ Source/Sync.js
@@ -12,9 +12,16 @@ var REST = function(type){
 var Sync = new Class({
     Extends: Request.JSON,
 
+    options: {
+        link: 'chain'
+    },
+
+    syncId: 0,
+
     sync: function(type, options, callback){
         if (callback && typeOf(callback) == 'function'){
-            this.attachEventOnce('sync', callback);
+            this.syncId++;
+            this.addEventOnce('sync', callback);
         }
 
         this[type ? type : 'update'](options);
@@ -22,12 +29,14 @@ var Sync = new Class({
         return this;
     },
 
-    attachEventOnce: function(type, fnc){
-        var once, cancel;
+    addEventOnce: function(type, fnc){
+        var syncId = this.syncId,
+            cancelType = type + ':' + (syncId),
+            once, cancel;
 
         cancel = function(){
             this.removeEvent(type, once);
-            this.removeEvent('cancel', cancel);
+            this.removeEvent(cancelType, cancel);
         };
 
         once = function(){
@@ -36,7 +45,7 @@ var Sync = new Class({
         };
 
         this.addEvent(type, once);
-        this.addEvent('cancel', cancel);
+        this.addEvent(cancelType, cancel);
 
         return this;
     },
@@ -45,9 +54,19 @@ var Sync = new Class({
 
     read: REST('GET'),
 
-    update: REST('PUT')
+    update: REST('PUT'),
 
-    // Delete already exists on Request
+    cancel: function(){
+        if (!this.running) {
+            this.fireEvent('cancel');
+            this.fireEvent('sync:' + (this.syncId) );
+            return this;
+        }
+
+        this.parent();
+        this.fireEvent('sync:' + (this.syncId) );
+        return this;
+    }
 });
 
 module.exports = Sync;
