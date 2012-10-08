b4ce463cb9d34debc0f8e4f41ccd550ffc04fe93
Garrick Cheung <garrick@garrickcheung.com>
2012-10-08 16:59:02 -0700

    Correct build.js

--- build.js
+++ build.js
@@ -10,7 +10,7 @@ var neuro = wrup.require('Neuro', './')
 
 var writeNeuro = function(){
     fs.writeFile(root + 'neuro-sync.js', src);
-    fs.writeFile(root + './neuro-sync-min.js', compressed);
+    fs.writeFile(root + 'neuro-sync-min.js', compressed);
     console.log('Neuro Sync created.');
 };
 
